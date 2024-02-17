c6288ccbdf6bc9f40849557f9e19bbaf1dc921fe
Paul Rose <ptrose78@gmail.com>
2024-02-17 08:07:25 -0600

    Created goalsSlice

---
+++ src/features/goalsSlice.js
@@ -0,0 +1,29 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+	goals: {}
+}
+
+export const goalsSlice = createSlice({
+	name: "goals",
+	initialState: initialState,
+	reducers: {
+		addGoal: (state, action) => {
+			const {id, name, date} = action.payload;
+			state.goals[id] = {
+				id: id,
+				name: name,
+				date: date,
+				taskIds: []
+			};
+		},
+		removeGoal: (state, action) => {
+			const {id, name, date} = action.payload;
+			state.goals.filter(goal => goal.id !== id)
+		}
+	}
+})
+
+export const {addGoal, removeGoal} = goalsSlice.actions;
+export const {selectedGoals} = (state) => state.goals.goals;
+export default goalsSlice.reducer;
