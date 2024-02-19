20b1b7abfe13be7afabd26cc2a4eefc62f58394c
Paul Rose <ptrose78@gmail.com>
2024-02-19 06:14:05 -0600

    Created NewTaskForm page and routing

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -2,7 +2,8 @@ import React from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import {selectGoals} from "../goals/goalsSlice";
-
+import ROUTES from "../../app/routes";
+import { Link } from "react-router-dom";
 
 export default function Tasks() {
     const goals = useSelector(selectGoals);
@@ -14,7 +15,9 @@ export default function Tasks() {
         <section>
             <h2>Goal: {goal.name}</h2>
             <h3>Task List</h3>
+            <Link to={ROUTES.newTaskRoute(goalId)}>
             <button>Create Task</button>
+            </Link>
         </section>
     )
 }
