2f7c931c44e90cd84a32e790aa18dd4bbd563d6d
Paul Rose <ptrose78@gmail.com>
2024-02-22 15:18:20 -0600

    Updated App Layout display for listing to-dos

--- src/app/AppLayout.js
+++ src/app/AppLayout.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import ROUTES from "./routes";
 import {useDispatch, useSelector} from "react-redux";
-import {selectGoals} from "../features/goals/goalsSlice"
+import {selectGoals} from "../features/goals/goalsSlice";
+import {selectTodos} from "../features/todos/todosSlice";
 
 
 export default function AppLayout() {
     const goals = useSelector(selectGoals);
+    const todos = useSelector(selectTodos);
     console.log(goals)
+    console.log(todos)
 
     return (
         <div>    
@@ -15,18 +18,30 @@ export default function AppLayout() {
                 <ul>
                 {Object.keys(goals).length === 0 ? (
                     <li> 
-                        <NavLink to={ROUTES.newGoalRoute()} >
-                        Create a Goal
+                        <NavLink to={ROUTES.newGoalRoute()}>
+                            Create a Goal
                         </NavLink>
                     </li>
                 ) : (
                     <li> 
-                        <NavLink to={ROUTES.goalsRoute()} >
-                         Goals
+                        <NavLink to={ROUTES.goalsRoute()}>
+                            Goals
                         </NavLink>
                     </li>
                 )}
+
+                {Object.values(todos).length !== 0 ? (
+                    <li>
+                        <NavLink to={ROUTES.todoRoute()}>
+                            To-dos
+                        </NavLink>
+                    </li>
+                ) : (
+                        null
+                )}
+
                 </ul>
+
             </nav>
             <Outlet/>
         </div>
